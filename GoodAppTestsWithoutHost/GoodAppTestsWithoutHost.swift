3c58d04f3bf6d6b4ea4daee26bde6c84ed6cf064
Paul Zabelin <paul.zabelin.c@udemy.com>
2018-06-12 16:19:21 -0700

    add failing to link test

--- GoodAppTestsWithoutHost/GoodAppTestsWithoutHost.swift
+++ GoodAppTestsWithoutHost/GoodAppTestsWithoutHost.swift
@@ -7,11 +7,13 @@
 //
 
 import XCTest
+@testable import GoodApp
 
 class GoodAppTestsWithoutHost: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        print(FirstViewController.self)
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
