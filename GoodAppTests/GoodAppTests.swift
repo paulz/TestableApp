7b0bebae3708858bc8c342927d9275b33eb10f7a
Paul Zabelin <paul.zabelin.c@udemy.com>
2018-06-12 15:26:56 -0700

    add sample test for first controller

--- GoodAppTests/GoodAppTests.swift
+++ GoodAppTests/GoodAppTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class GoodAppTests: XCTestCase {
     
+    let subject = FirstViewController()
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
